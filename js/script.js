8bbfe42549599910f290221147b2911a2c40d9bf
galactus299 <mianshayanbabar@gmail.com>
2022-02-07 16:38:19 +0500

    NPM Scripts Part 2

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#carouselButton").click(function(){
+            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                $("#mycarousel").carousel('pause');
+                $("#carouselButton").children("span").removeClass('fa-pause');
+                $("#carouselButton").children("span").addClass('fa-play');
+            }
+            else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                $("#mycarousel").carousel('cycle');
+                $("#carouselButton").children("span").removeClass('fa-play');
+                $("#carouselButton").children("span").addClass('fa-pause');                    
+            }
+        });
+    $("#login-button").click(function(){
+        $('#loginModal').modal('show');
+    });
+    $("#reservertable-button").click(function(){
+        $("#ReserveTable").modal('show');
+    });
+    
+});
